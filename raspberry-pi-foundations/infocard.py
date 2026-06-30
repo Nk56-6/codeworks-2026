907e690df03371398f5f076f62566f69c3d54f12
Nat <suter.king.family.139@gmail.com>
2026-06-30 14:16:42 -0400

    added my info

---
+++ raspberry-pi-foundations/infocard.py
@@ -0,0 +1,15 @@
+name = "Nat King"
+age = "16"
+
+interests = "gaming, drawing, and the ocean"
+why_here = "I joined because I enjoy building circuits and messing with technology"
+goals = "I want to learn how to make circuits with Raspberry Pi"
+personality = "focused listener"
+
+print("Name:", name)
+print("Age:", age)
+
+print("interests:", interests)
+print("Why I'm here:", why_here)
+print("Goals:", goals)
+print("Personality", personality)
